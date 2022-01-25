227e77006d0ea2c18e087f03f680cb25a74cc09d
Òscar Herran <73947491+oherranmo@users.noreply.github.com>
2022-01-25 18:42:45 +0100

    Update Google Chrome Gestionat.sh

--- Google Chrome Gestionat.sh
+++ Google Chrome Gestionat.sh
@@ -59,12 +59,12 @@ chrome()
 if [[ $(getconf LONG_BIT) = "64" ]]
 then
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    dpkg -i google-chrome-stable_current_amd64.deb
     rm -f google-chrome-stable_current_amd64.deb
     check
 else
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
-    sudo dpkg -i google-chrome-stable_current_i386.deb
+    dpkg -i google-chrome-stable_current_i386.deb
     rm -f google-chrome-stable_current_i386.deb
     check
 fi
